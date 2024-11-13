69ca14cdd591cd62c9ce6561af96ac251e4228c1
surya <varma.surya@protonmail.com>
2024-11-12 20:35:09 -0500

    config font and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Poppins } from "next/font/google";
+import type { Metadata } from 'next';
+import './globals.css';
+import { Poppins } from 'next/font/google';
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-poppins",
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-poppins',
 });
 
 export const metadata: Metadata = {
-  title: "Store-It",
-  description: "Perosonalized Storage System",
+  title: 'Store-It',
+  description: 'Perosonalized Storage System',
 };
 
 export default function RootLayout({
